7ff05c5aea2680cb42933ecef0e49d2ba148750e
Great Brighton <ekuayovwegreat@gmail.com>
2022-11-01 07:32:12 +0000

    Search Engine

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,18 +1,4 @@
-ray
- * @value: value to search in
- * Return: index of the number
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int index;
-
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
-}#include "search_algos.h"
+#include "search_algos.h"
 
 /**
  * recursive_search - searches for a value in an array of
@@ -58,4 +44,18 @@ int recursive_search(int *array, size_t size, int value)
  * the index of the number
  *
  * @array: input array
- * @size: size of the ar
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}
